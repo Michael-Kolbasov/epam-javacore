12e90b915ac3fd3c65c5a552a8e5fdfb9d7380e4
Michael-Kolbasov <trololoshenkilolo@yandex.ru>
2018-10-17 15:11:36 +0400

    Initial upload.

--- task04/recursion/test/root/solution/AmountOfDigitsTest.java
+++ task04/recursion/test/root/solution/AmountOfDigitsTest.java
@@ -1,4 +1,24 @@
 package root.solution;
+import org.junit.Test;
+import org.junit.Before;
+import static org.junit.Assert.assertEquals;
 
 public class AmountOfDigitsTest {
+    private AmountOfDigits amountOfDigits;
+
+    @Before
+    public void setUp() {
+        amountOfDigits = new AmountOfDigits(1);
+    }
+
+    @Test
+    public void testFindPower() {
+        assertEquals(2, amountOfDigits.findAmountOfDigits(23));
+        assertEquals(1, amountOfDigits.findAmountOfDigits(0));
+        assertEquals(1, amountOfDigits.findAmountOfDigits(1));
+        assertEquals(2, amountOfDigits.findAmountOfDigits(-10));
+        assertEquals(6, amountOfDigits.findAmountOfDigits(332211));
+        assertEquals(10, amountOfDigits.findAmountOfDigits(1234567890));
+        assertEquals(2, amountOfDigits.findAmountOfDigits(2*10));
+    }
 }
--- task04/recursion/test/root/solution/SumOfDigitsTest.java
+++ task04/recursion/test/root/solution/SumOfDigitsTest.java
@@ -1,4 +1,25 @@
 package root.solution;
+import org.junit.Test;
+import org.junit.Before;
+import static org.junit.Assert.assertEquals;
 
 public class SumOfDigitsTest {
+    private SumOfDigits sumOfDigits;
+
+    @Before
+    public void setUp() {
+        sumOfDigits = new SumOfDigits(1);
+    }
+
+    @Test
+    public void testFindPower() {
+        assertEquals(5, sumOfDigits.findSumOfDigits(23));
+        assertEquals(0, sumOfDigits.findSumOfDigits(0));
+        assertEquals(1, sumOfDigits.findSumOfDigits(-1));
+        assertEquals(15, sumOfDigits.findSumOfDigits(12345));
+        assertEquals(15, sumOfDigits.findSumOfDigits(-12345));
+        assertEquals(1, sumOfDigits.findSumOfDigits(10000));
+        assertEquals(2, sumOfDigits.findSumOfDigits(200));
+        assertEquals(19, sumOfDigits.findSumOfDigits(9091));
+    }
 }
