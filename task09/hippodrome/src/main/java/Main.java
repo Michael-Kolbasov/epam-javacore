4e74fb03cf9416c447d669b09f7aeef7ab95799f
Michael-Kolbasov <trololoshenkilolo@yandex.ru>
2018-11-21 21:15:22 +0400

    Added author teg

--- task09/hippodrome/src/main/java/Main.java
+++ task09/hippodrome/src/main/java/Main.java
@@ -3,6 +3,8 @@
  * Create a hippodrome.
  *  Player makes a bet on a horse. Horses have their own constant speed and random acceleration speed.
  *  Each running horse represents a unique stream, so that all horses run in a parallel fashion.
+ *
+ * @author Michael Kolbasov
  */
 public class Main {
 
