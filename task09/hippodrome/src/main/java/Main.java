767cfefe2673783ac1acf6cc0e4815d63321b96a
Michael-Kolbasov <trololoshenkilolo@yandex.ru>
2018-11-21 17:27:27 +0400

    Added description of the task

--- task09/hippodrome/src/main/java/Main.java
+++ task09/hippodrome/src/main/java/Main.java
@@ -1,3 +1,9 @@
+/**
+ * Task #9
+ * Create a hippodrome.
+ *  Player makes a bet on a horse. Horses have their own constant speed and random acceleration speed.
+ *  Each running horse represents a unique stream, so that all horses run in a parallel fashion.
+ */
 public class Main {
 
     public static void main(String[] args) {
