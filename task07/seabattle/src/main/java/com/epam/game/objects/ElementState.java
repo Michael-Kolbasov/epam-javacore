f0184142d93ce0c3920d3b52132d81be836ff0d5
Michael-Kolbasov <trololoshenkilolo@yandex.ru>
2018-11-25 17:22:24 +0400

    Added more descriptive comments

--- task07/seabattle/src/main/java/com/epam/game/objects/ElementState.java
+++ task07/seabattle/src/main/java/com/epam/game/objects/ElementState.java
@@ -3,8 +3,8 @@ package com.epam.game.objects;
 /**
  * This enum provides the cells' state.
  * {@code WATER} for initial map filling.
- * {@code BORDER} for surrounding cells on the map upon ship put.
- * {@code SHIP} for the map cells' and ships' objects when upon ship put.
+ * {@code BORDER} for surrounding cells on the map upon ship has been put.
+ * {@code SHIP} for the map cells' and ships' objects upon ship has been put.
  */
 public enum ElementState {
     WATER, BORDER, SHIP
